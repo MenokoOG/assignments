691d02e8acf728675671feca833f886616b0385a
Lawrence Jefferson II <jefftkddan@gmail.com>
2024-01-04 09:04:19 -0800

    more lessons

---
+++ exercises/select_multiple_ele/index.js
@@ -0,0 +1,7 @@
+let things = document.getElementsByClassName("things")
+for (let i = 0;i < things.length; i++){
+    console.log(things[i].innerText)
+}
+// console.log(things[0])
+// console.log(things)
+// console.dir(things)
