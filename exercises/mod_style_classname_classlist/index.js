ba1ff636bf763bb2bb0df378878b3910742db41e
Lawrence Jefferson II <jefftkddan@gmail.com>
2024-01-04 13:32:14 -0800

    more lessons

---
+++ exercises/mod_style_classname_classlist/index.js
@@ -0,0 +1,10 @@
+var header = document.getElementById("header")
+header.style.backgroundColor = "blue"
+
+
+document.body.style.backgroundColor = "burlywood"
+// fontSize
+// background-color
+// display
+// textAlign
+// margin, padding, border
