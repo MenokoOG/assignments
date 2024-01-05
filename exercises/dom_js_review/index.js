4b0abf0687fc8402b2dd8248088c93473270a4e2
Lawrence Jefferson II <jefftkddan@gmail.com>
2024-01-04 19:29:25 -0800

    more lessons

--- exercises/dom_js_review/index.js
+++ exercises/dom_js_review/index.js
@@ -5,3 +5,53 @@
  * 
  * Make sure to read through the `notes.md` file for a recap and some new information regarding using the DOM API
  */
+
+let head1El = document.getElementById("header1")
+let head2El = document.querySelector("#header2")
+let olEl = document.querySelectorAll("#ol-el > li")
+let navEl = document.querySelectorAll("#navbar > li")
+// console.log(Array.from(navEl))
+
+head1El.textContent = "new heading change"
+head1El.style.color = "yellow"
+head1El.style.backgroundColor = "black"
+head1El.style.border = "2px solid red"
+head1El.style.textAlign = "center"
+head1El.style.padding = "20px 20px"
+
+head2El.textContent = "Hello World!!!!!!"
+head2El.style.color = "green"
+head2El.style.border = "1px dashed purple"
+head2El.style.backgroundColor = "pink"
+head2El.style.display = "flex"
+head2El.style.justifyContent = "center"
+head2El.style.padding = " 10px 30px "
+
+
+for ( let i = 0; i < olEl.length; i++){
+    olEl[i].textContent = "**Hello Menoko OG***"
+    olEl[i].style.color = "purple"
+    olEl[i].style.fontWeight = "bold"
+    olEl[i].style.fontSize = "20px"
+}
+
+
+
+
+
+
+
+for ( let i = 0; i < navEl.length; i++){
+    navEl[i].textContent = "**Not going to tell you!***"
+    navEl[i].style.color = "red"
+    navEl[i].style.fontWeight = "bold"
+    navEl[i].style.fontSize = "40px"
+}
+
+let newPara = document.createElement("p")
+newPara.textContent = "This was created in JavaScript"
+newPara.style.color = "blue"
+newPara.style.fontWeight = "bold"
+newPara.style.textAlign = "center"
+newPara.style.fontSize = "20px"
+document.body.append(newPara)
---
+++ exercises/self_study_get_value/index.js
@@ -0,0 +1,5 @@
+// get the user input and console log the information put in.
+inputEl = document.getElementById("input-el")
+btnEl = document.getElementById("btn").addEventListener("click", function(){
+    console.log(inputEl.value)
+})
