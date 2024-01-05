7fbdbb1bec3c7d3c5838022ff680ed9d5b319bcf
Lawrence Jefferson II <jefftkddan@gmail.com>
2024-01-05 10:28:10 -0800

    added function for color change on key press

--- exercises/event_listen_practice/index.js
+++ exercises/event_listen_practice/index.js
@@ -6,28 +6,50 @@ function mouseOver() {
 }
 squareEL.onmouseover = mouseOver
 
-function mouseDown(){
+function mouseDown() {
     squareEL.style.backgroundColor = "red"
 }
 squareEL.onmousedown = mouseDown
 
-function mouseLetGo(){
+function mouseLetGo() {
     squareEL.style.backgroundColor = "yellow"
 }
 squareEL.onmouseup = mouseLetGo
 
-function mouseDouble(){
+function mouseDouble() {
     squareEL.style.backgroundColor = "green"
 }
 squareEL.ondblclick = mouseDouble
 
-function mouseScroll(){
+function mouseScroll() {
     squareEL.style.backgroundColor = "orange"
 }
 
 squareEL.onwheel = mouseScroll
 
+//Function to change the background color based on the pressed key
+function changeColor(event) {
+    switch (event.key) {
+        case 'r':
+            squareEL.style.backgroundColor = 'red';
+            break;
+        case 'y':
+            squareEL.style.backgroundColor = 'yellow';
+            break;
+        case 'o':
+            squareEL.style.backgroundColor = 'orange';
+            break;
+        case 'g':
+            squareEL.style.backgroundColor = 'green';
+            break;
+        default:
+            // Do nothing for other keys
+            break;
+    }
+}
 
+// Add an event listener to the document to listen for key presses
+document.addEventListener('keydown', changeColor);
 
 
 
