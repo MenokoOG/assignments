6c2db544abb0d4a5368dfa41d41a48a832db56fa
Lawrence Jefferson II <jefftkddan@gmail.com>
2024-01-18 12:00:15 -0800

    V School Lesson update Menoko OG Date: 1-18-24

---
+++ code_assess.js
@@ -0,0 +1,9 @@
+// Write a function that takes an array of numbers and returns a new array containing only the even numbers.
+const numbers = [1, 2, 3, 4, 5, 6];
+const filterEvenNumbers = numbers.filter( num => num % 2 === 0) 
+console.log(filterEvenNumbers)
+
+
+
+
+
