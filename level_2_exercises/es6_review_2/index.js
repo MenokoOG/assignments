a0e23a04055d5e094c1a8f6aed299247b9076de6
Lawrence Jefferson II <jefftkddan@gmail.com>
2024-01-19 09:01:35 -0800

    V School Lesson update Menoko OG Date: 1-19-24

--- level_2_exercises/es6_review_2/index.js
+++ level_2_exercises/es6_review_2/index.js
@@ -1,1 +1,55 @@
 // es6 part 2 review, menoko og, 1-19-24
+const button = document.getElementById('button')
+const output = document.getElementById('output')
+const input = document.getElementById('input')
+// change this function to be an arrow function
+// button.addEventListener('click', function(){
+//     output.innerText = "You've submitted: " + input.value
+// })
+
+const btnClick = button.addEventListener("click", () => output.innerHTML = `You've submitted ${input.value}`)
+
+//-------------------------------------------------------------------------------------------
+// Use the Rest Operator to help this function return an array of animals, no matter how many animals are passed to it:
+const collectAnimals = (...animals) => {  
+    return animals;
+}
+
+console.log(collectAnimals("dog", "cat", "mouse", "jackolope", "platypus")); 
+// ["dog", "cat", "mouse", "jackolope", "platypus"]
+
+//-------------------------------------------------------------------------------------------
+// Use destructuring to use the property names as variables:
+
+const vacation = {  
+  location: "Burly Idaho",
+  duration: "2 weeks"
+};
+
+function parseSentence({ location, duration }){
+  return `We're going to have a good time in ${location} for ${duration}`
+}
+
+console.log(parseSentence(vacation)) // this line will break stuff unless you fix the parameter
+
+//-------------------------------------------------------------------------------------------
+// Make the following function more ES6xy. Use at least both the rest and spread operators:
+// this function should add as many items to the front of the returned array as the caller of the function wants
+// function unshift(array, a, b, c, d, e) {  
+//   return [a, b, c, d, e].concat(...array);
+// }
+function unshift(array, ...items) {
+    return [...items, ...array].sort();
+  }
+
+const originalArry = ["a","b", "c", "d", "e"]
+const newArray = unshift(originalArry, "f", "g", "h" )
+console.log(newArray)
+
+//-------------------------------------------------------------------------------------------
+// make "greeting" default to "hello"
+function greet(name, greeting = "hello"){
+    return `${greeting} ${name}`
+}
+
+console.log(greet('Emily'))
