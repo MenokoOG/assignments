d2b54626a585449e077b4a2f8e5e54189f8e3bbb
Lawrence Jefferson II <jefftkddan@gmail.com>
2024-01-20 14:48:06 -0800

    V School Lesson update Menoko OG Date: 1-20-24

--- level_2_exercises/es6_practice/index.js
+++ level_2_exercises/es6_practice/index.js
@@ -1,1 +1,129 @@
 // es6 practice, menoko og, 1-19-24
+// Replace all the `var`s with `let` and `const`. Alter the code however necessary to make sure this continues to work (so the pet's name **isn't** `"John"`, but instead `"spot"` is returned). You only need to delete `var` and insert `let` and `const`
+
+// John is the pet owner, and his name should be stored differently than the other names.
+
+const ownerName = "John";
+let age = 101;
+
+function runForLoop(pets) {
+    const petObjects = [];
+    for (let i = 0; i < pets.length; i++) {
+        const pet = { type: pets[i] };
+        let name;
+        if (pets[i] === "cat") {
+            name = "fluffy";
+        } else {
+            name = "spot";
+        }
+        console.log("pet name: ", name);
+        pet.name = name;
+        petObjects.push(pet);
+    }
+    console.log("man name: ", ownerName);
+    return petObjects;
+}
+
+runForLoop(["cat", "dog"]);
+
+// Re-write this `.map()` using an arrow function:
+
+// Be aware that if JavaScript sees a `{` directly after the `=>` it will think it's starting a function, and not starting an object, so the `:` will be an unexpected symbconst carrots = ["bright orange", "ripe", "rotten"];
+
+const carrots = ["bright orange", "ripe", "rotten"];
+
+const mapVegetables = (arr) => arr.map(carrot => ({ type: "carrot", name: carrot }));
+console.log(mapVegetables(carrots))
+
+// Re-write this .filter() ’s callback function using an arrow function:
+const people = [
+    {
+        name: "Princess Peach",
+        friendly: false
+    },
+    {
+        name: "Luigi",
+        friendly: true
+    },
+    {
+        name: "Mario",
+        friendly: true
+    },
+    {
+        name: "Bowser",
+        friendly: false
+    }
+]
+
+const filterForFriendly = arr => arr.filter(person => person.friendly);
+console.log(filterForFriendly(people))
+
+// Re-write the following functions to be arrow functions:
+
+const doMathSum = (a, b) => a + b
+
+const produceProduct = (a, b) => a * b
+console.log(doMathSum(5, 4))
+console.log(produceProduct(5 ,5))
+
+// Write a printString function that takes firstName, lastName, and age as parameters and returns a string like the following:
+
+// firstName should default to "Jane"lastName should default to "Doe"age should default to 100
+
+// Hi Kat Stark, how does it feel to be 40?
+function printString(firstName = "Jane", lastName = "Doe", age = 100) {
+    return `Hi ${firstName} ${lastName}, how does it feel to be ${age}?`;
+}
+
+const result = printString("Kat", "Stark", 40);
+console.log(result);
+
+// Use the shorthand syntax to make the following filter take up one line. Copy and paste the array to test it.
+
+const animals = [
+    {
+        type: "dog",
+        name: "theodore"
+    },
+    {
+        type: "cat",
+        name: "whiskers"
+    },
+    {
+        type: "pig",
+        name: "piglette"
+    },
+    {
+        type: "dog",
+        name: "sparky"
+    }
+ ];
+ 
+//  function filterForDogs(arr) {
+//      return arr.filter(animal => {
+//          if (animal.type === "dog") {
+//              return true
+//          } else {
+//              return false
+//          }
+//      })
+//  }
+
+ const filterForDogs = arr => arr.filter(animal => animal.type === "dog");
+ console.log(filterForDogs(animals))
+//  Using template literals, write a function that takes location and name parameters and outputs a message formatted like this:
+//  Hi Janice!
+ 
+//  Welcome to Hawaii.
+ 
+//  I hope you enjoy your stay. Please ask the president of Hawaii if you need anything.
+ function welcomeMessage(location, name) {
+    return `Hi ${name}!\n\nWelcome to ${location}.\n\nI hope you enjoy your stay. Please ask the president of ${location} if you need anything.`;
+}
+
+
+const message = welcomeMessage("Hawaii", "Janice");
+console.log(message);
+
+
+
--- level_2_exercises/slope_style/index.js
+++ level_2_exercises/slope_style/index.js
@@ -1,1 +1,105 @@
 // slope style assignment, menoko og
+
+// Use the Rest Operator to help this function return an array of animals, no matter how many animals are passed to it:
+function collectAnimals(...animals) {
+    return animals;
+}
+
+
+const animalArray = collectAnimals("dog", "cat", "mouse", "jackolope", "platypus");
+console.log(animalArray);
+
+// Write a function that returns a food object with the array names as properties. You'll use an ES6 shorthand syntax that becomes useful when a variable name is mentioned twice in both the name and value of properties in your object:
+function combineFruit(fruit, sweets, vegetables) {
+    return {
+        fruit,
+        sweets,
+        vegetables
+    };
+}
+
+console.log(combineFruit(["apple", "pear"], ["cake", "pie"], ["carrot"]));
+
+// Use destructuring to use the property names as variables. Destructure the object in the parameter:
+function parseSentence({ location, duration }) {
+    return `We're going to have a good time in ${location} for ${duration}`;
+  }
+  
+  console.log(parseSentence({
+    location: "Burly Idaho",
+    duration: "2 weeks"
+  }));
+
+//   Use array destructuring to make this code ES6:
+function returnFirst(items) {
+    const [firstItem] = items;
+    return firstItem;
+}
+
+
+const itemsArray = [1, 2, 3, 4, 5];
+console.log(returnFirst(itemsArray)); 
+
+// Write destructuring code to assign variables that will help us return the expected string. Also, change the string to be using Template literals:
+
+const favoriteActivities = ["magnets", "snowboarding", "philanthropy", "janitor work", "eating"];
+
+function returnFavorites(arr) {
+    const [firstFav, secondFav, thirdFav] = arr;
+    return `My top three favorite activities are, ${firstFav}, ${secondFav}, and ${thirdFav}`;
+}
+
+console.log(returnFavorites(favoriteActivities));
+
+
+function combineAnimals(...arrays) {
+    return [...arrays.flat()];
+}
+
+// Use the Rest and Spread Operator to help take any number of arrays, and return one array. You will need to change how the arrays are passed in. You may write it assuming you will always recieve three arrays if you would like to.
+const realAnimals = ["dog", "cat", "mouse"];
+const magicalAnimals = ["jackalope"];
+const mysteriousAnimals = ["platypus"];
+
+const combinedAnimals = combineAnimals(realAnimals, magicalAnimals, mysteriousAnimals);
+console.log(combinedAnimals);
+
+// more es6 functions
+
+const product = (...numbers) => numbers.reduce((acc, number) => acc * number, 1);
+
+const unshift = (array, ...elements) => [...elements, ...array];
+
+const result = product(2, 3, 4, 5, 6);
+console.log(result); 
+const originalArray = [1, 2, 3, 4, 5];
+const newArray = unshift(originalArray, 6, 7, 8, 9, 10);
+console.log(newArray); 
+
+
+function populatePeople(names) {
+    return names.map(function (name) {
+        [firstName, lastName] = name.split(" ");
+        return {
+            firstName,
+            lastName
+        };
+    });
+}
+// Write some destructuring code to help this function out. Use the ES6 shorthand that helps make the syntax look less redundant to simplify it:
+
+
+const peopleArray = populatePeople(["Frank Peterson", "Suzy Degual", "Liza Jones"]);
+console.log(peopleArray);
+// Output:
+// [
+//   { firstName: "Frank", lastName: "Peterson" },
+//   { firstName: "Suzy", lastName: "Degual" },
+//   { firstName: "Liza", lastName: "Jones" },
+// ]
+
+
+
+
+  
+  
