04d627d8162c9df1293b9298879a4ac946a0494d
Lawrence Jefferson II <jefftkddan@gmail.com>
2024-01-23 10:25:28 -0800

    V School Lesson update Menoko OG Date: 1-23-24

--- level_2_exercises/axios_get/index.js
+++ level_2_exercises/axios_get/index.js
@@ -1,10 +1,21 @@
 // axios get, menoko og, 1-22-24
 
 // Get requests with axios
-// url: https://api.vschool.io/<yourname>/todo
+// url: https://api.vschool.io/lawrencejefferson/todo
 
 // Get All
-
+axios.get("https://api.vschool.io/lawrencejefferson/todo")
+    .then(response =>{
+        for(let i =0; i < response.data.length; i++){
+            const h1 = document.createElement("h1")
+            h1.textContent = response.data[i].title
+            document.body.appendChild(h1)
+        }
+    })
+    .catch(error => console.log(error))
 
 
 // Get One
+axios.get("https://api.vschool.io/lawrencejefferson/todo/65aea9f978fe452c114b0604")
+    .then(response => console.log(response.data))
+    .catch(error => console.log(error))
---
+++ level_2_exercises/pokemon_list/index.js
@@ -0,0 +1,48 @@
+// pokemon list assignment, menoko og, 1-23-24
+
+// Step One - Get the data
+function getPokemonData() {
+    axios.get('https://api.vschool.io/pokemon')
+        .then(response => {
+            // Assuming your data structure is response.data.objects
+            const pokemonArray = response.data.objects[0].pokemon
+            
+            // Step Two - Display the data
+            displayPokemonNames(pokemonArray)
+        })
+        .catch(error => {
+            console.error('Error fetching Pokémon data:', error)
+        });
+}
+
+// Step Two - Display the data
+function displayPokemonNames(pokemonArray) {
+    const body = document.body
+
+    // Create a new <ul> element
+    const pokemonList = document.createElement('ul')
+
+    //  each item in pokemonArray is an object with a name property
+    for (const pokemonObj of pokemonArray) {
+        // Access the name property
+        const pokemonName = pokemonObj.name
+
+        // Create a new <li> element for each Pokémon's name
+        const listItem = document.createElement('li')
+        listItem.textContent = pokemonName
+
+        // Append the <li> element to the <ul>
+        pokemonList.appendChild(listItem)
+    }
+
+    // Append the <ul> element to the body of the document
+    body.appendChild(pokemonList)
+}
+
+// Call the function to get Pokémon data
+getPokemonData()
+
+
+
+
+
