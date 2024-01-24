de625f55aa81c1920e7626f9c07833469533d500
Lawrence Jefferson II <jefftkddan@gmail.com>
2024-01-24 14:06:02 -0800

    V School Lesson update Menoko OG Date: 1-24-24 version 2

--- level_2_exercises/axios_todo_project/app.js
+++ level_2_exercises/axios_todo_project/app.js
@@ -4,12 +4,12 @@
 // use es5 syntax with es6 callback for main function getters for browser parsing compatibilty 
 
 // Define the base URL for the API and references to HTML elements
-const baseURL = "https://api.vschool.io/lawrencejefferson/todo/";
-const form = document.querySelector('form');
-const submit = document.getElementById('submitBtn'); 
-const listWrapper = document.getElementById('list-wrapper');
+const baseURL = "https://api.vschool.io/lawrencejefferson/todo/"
+const form = document.querySelector('form')
+const submit = document.getElementById('submitBtn')
+const listWrapper = document.getElementById('list-wrapper')
 // Fetch todos when the page loads
-getTodos();
+getTodos()
 // Function to add a new todo
 function postTodo() {
     if (form.title.value) {
@@ -18,12 +18,12 @@ function postTodo() {
         // Send a POST request to the API
         axios.post(baseURL, newPackage).then(response => renderResponse(response.data));
         // Reset the form and focus on the title input
-        form.reset();
-        form.title.focus();
+        form.reset()
+        form.title.focus()
     } else {
         // Display an alert if title is not provided
-        alert("A title is required to submit this todo.");
-        form.title.focus();
+        alert("A title is required to submit this todo.")
+        form.title.focus()
     }
 }
 // Function to get all todos
@@ -32,48 +32,48 @@ function getTodos() {
 }
 // Function to loop through todos and render them
 function loopandRenderTodos(response) {
-    response.data.forEach(todoObject => renderResponse(todoObject));
+    response.data.forEach(todoObject => renderResponse(todoObject))
 }
 // Constructor function to create a Package object
 function Package() {
-    this.title = form.title.value;
-    this.description = form.desc.value;
-    this.price = form.price.value;
-    this.imgUrl = form.imgUrl.value;
-    this.completed = form.completed.checked;
+    this.title = form.title.value
+    this.description = form.desc.value
+    this.price = form.price.value
+    this.imgUrl = form.imgUrl.value
+    this.completed = form.completed.checked
 }
 // Function to render a todo item
 function renderResponse(response) {
     let rendered = new RenderObject(response);
-    let itemWrapper = document.createElement('div');
-    itemWrapper.classList = "item-wrapper";
-    itemWrapper.id = rendered.id;
-    let number = document.createElement('p');
-    number.classList = "number";
-    number.textContent = String(listWrapper.children.length + 1) + ".";
-    let deleteX = document.createElement('p');
-    deleteX.classList = "deleteX";
-    deleteX.textContent = "X";
-    deleteX.addEventListener("click", deleteTodo);
-    itemWrapper.appendChild(number);
-    itemWrapper.appendChild(deleteX);
+    let itemWrapper = document.createElement('div')
+    itemWrapper.classList = "item-wrapper"
+    itemWrapper.id = rendered.id
+    let number = document.createElement('p')
+    number.classList = "number"
+    number.textContent = String(listWrapper.children.length + 1) + "."
+    let deleteX = document.createElement('p')
+    deleteX.classList = "deleteX"
+    deleteX.textContent = "X"
+    deleteX.addEventListener("click", deleteTodo)
+    itemWrapper.appendChild(number)
+    itemWrapper.appendChild(deleteX)
 
     for (key in rendered) {
-        let item = document.createElement(rendered[key].elementType);
+        let item = document.createElement(rendered[key].elementType)
         if (rendered[key].elementType === "img") {
-            item.classList = rendered[key].class;
-            item.src = rendered[key].value;
+            item.classList = rendered[key].class
+            item.src = rendered[key].value
         } else {
-            item.classList = rendered[key].class;
-            item.textContent = rendered[key].value;
+            item.classList = rendered[key].class
+            item.textContent = rendered[key].value
             if (rendered[key].holder !== undefined) {
-                item.value = rendered[key].holder;
-                item.addEventListener("click", updateTodo);
+                item.value = rendered[key].holder
+                item.addEventListener("click", updateTodo)
             }
         }
-        itemWrapper.appendChild(item);
+        itemWrapper.appendChild(item)
     }
-    listWrapper.appendChild(itemWrapper);
+    listWrapper.appendChild(itemWrapper)
 }
 // Constructor function to create a RenderObject
 function RenderObject(obj) {
@@ -103,15 +103,15 @@ function RenderObject(obj) {
         class: (obj.completed ? "green" : "red") + " complete-check",
         holder: obj.completed ? true : false,
     };
-    this.id = obj._id;
+    this.id = obj._id
 }
 // Event listener for the submit button
-submit.addEventListener("click", postTodo);
+submit.addEventListener("click", postTodo)
 // Function to delete a todo
 function deleteTodo(e) {
-    const id = e.target.parentNode.id;
-    e.target.parentNode.remove();
-    axios.delete(baseURL + id).then(changeItemIndex, () => alert("There was a problem deleting your todo :("));
+    const id = e.target.parentNode.id
+    e.target.parentNode.remove()
+    axios.delete(baseURL + id).then(changeItemIndex, () => alert("There was a problem deleting your todo :("))
 }
 // Function to update a todo
 function updateTodo(e) {
@@ -121,22 +121,22 @@ function updateTodo(e) {
     };
     axios.put(baseURL + id, update).then(response => {
         if (response.data.completed) {
-            e.target.classList = "green complete-check";
+            e.target.classList = "green complete-check"
             e.target.value = true;
-            e.target.textContent = "✓ Completed";
+            e.target.textContent = "✓ Completed"
         } else {
-            e.target.classList = "red complete-check";
+            e.target.classList = "red complete-check"
             e.target.value = false;
-            e.target.textContent = "X Not Complete";
+            e.target.textContent = "X Not Complete"
         }
-        (response.data.completed ? "green" : "red") + " complete-check";
-    });
+        (response.data.completed ? "green" : "red") + " complete-check"
+    })
 }
 // Function to update the index of todo items after deletion, start one otherwise function will not work properly.
 function changeItemIndex() {
     for (let i = 0; i < listWrapper.children.length + 1; i++) {
         if (listWrapper.childNodes[i].childNodes[0]) {
-            listWrapper.childNodes[i].childNodes[0].textContent = i + ".";
+            listWrapper.childNodes[i].childNodes[0].textContent = i + "."
         }
     }
 }
