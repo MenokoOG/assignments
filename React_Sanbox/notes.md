d467ad7c4358e0f6041fe03e7ef31cb26f25f76d
Lawrence Jefferson II <jefftkddan@gmail.com>
2024-01-25 17:02:01 -0800

    V School Lesson update Menoko OG Date: 1-25-24

--- React_Sanbox/notes.md
+++ React_Sanbox/notes.md
@@ -1,1 +1,2 @@
 This is repository for my react practice for V- school
+using code spaces and will download the zips and store them here.
---
+++ level_3_react/run_local/run local/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const newH1 = useState(0)
+
+  return (
+    <>
+     
+      <h1>This is a test in vite and react</h1>
+      
+        
+      
+      
+    </>
+  )
+}
+
+export default App
