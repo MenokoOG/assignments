194992c50b714115a15e81a73c8ead3f285b532d
Lawrence Jefferson II <jefftkddan@gmail.com>
2024-01-29 12:49:01 -0800

    V School Lesson update Menoko OG Date: 1-19-24

--- React_Sanbox/airbnb/src/index.jsx
+++ React_Sanbox/airbnb/src/index.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import Navbar from "./Navbar.jsx";
 import "./index.css";
 import Hero from "./Hero.jsx";
+import Card from "./Card.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -10,6 +11,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <div className="div">
     <Navbar />
     <Hero />
+    <Card />
     </div>
     </div>
   </React.StrictMode>
