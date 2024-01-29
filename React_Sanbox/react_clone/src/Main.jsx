7d288fa28e9cdec863ffa8e148a2a0a6ccf598c8
Lawrence Jefferson II <jefftkddan@gmail.com>
2024-01-29 09:33:05 -0800

    V School Lesson update Menoko OG Date: 1-29-24

--- React_Sanbox/react_clone/src/Main.jsx
+++ React_Sanbox/react_clone/src/Main.jsx
@@ -1,9 +1,20 @@
 import './Main.css'
+// import reactMainImg from "./react-icon-large.png"
 
 function Main(){
     return (
         <>
-        <h1>Main content here!!!</h1>
+        <main>
+        {/* <img src={reactMainImg} className="main--icon" /> */}
+            <h1 className='main--title'>Fun facts about React</h1>
+            <ul className='main--facts'>
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100K stars on GitHub</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </main>
         </>
 
     )
--- React_Sanbox/react_clone/src/Navbar.jsx
+++ React_Sanbox/react_clone/src/Navbar.jsx
@@ -1,9 +1,10 @@
 import "./Navbar.css";
+import reactImg1 from "./react-icon-small.png"
 
 function Navbar() {
   return (
     <nav>
-      <img src="../public/download.png" className="nav--icon" />
+      <img src={reactImg1} className="nav--icon" />
       <h3 className="nav--logo_text">ReactFacts</h3>
       <h4 className="nav--title">React Course - Project 1</h4>
     </nav>
