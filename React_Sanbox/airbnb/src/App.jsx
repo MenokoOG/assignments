65e18efdf59fec8bbad0c8e3bd7572a8e27e9da0
Lawrence Jefferson II <jefftkddan@gmail.com>
2024-01-31 18:12:12 -0800

    V School Lesson update Menoko OG Date: 1-31-24

--- React_Sanbox/airbnb/src/App.jsx
+++ React_Sanbox/airbnb/src/App.jsx
@@ -1,24 +1,40 @@
 import React from "react";
-import katie from "./images/katie-zaferes.png"
 import Navbar from "./components/Navbar.jsx";
 import Hero from "./components/Hero.jsx";
 import Card from "./components/Card.jsx";
+import cardData from "./data.js";
+/*
+Challenge:
 
-function App(){
+- import the array of data from data.js
+- map over the array to create <Card /> components
+- display the array of card components under the navbar
+  (in place of the current <Card /> component)
+
+Note: We haven't styled the group of components yet, so they'll
+still be block elements, stacked vertically. We'll add styling later.
+*/
+function App() {
+  const cardElements = cardData.map((card) => {
     return (
-        <div>
-        <Navbar />
-        < Hero />
-        <Card 
-            img={katie}
-            rating="5.0"
-            reviewCount={6}
-            country="USA"
-            title="Life Lessons with Katie Zaferes"
-            price={136}
-        />
+      <Card
+        img={card.coverImg}
+        rating={card.stats.rating}
+        reviewCount={card.stats.reviewCount}
+        location={card.location}
+        title={card.title}
+        price={card.price}
+      />
+    );
+  });
+
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      {cardElements}
     </div>
-    )
+  );
 }
 
-export default App
+export default App;
--- React_Sanbox/airbnb/src/components/Card.jsx
+++ React_Sanbox/airbnb/src/components/Card.jsx
@@ -3,12 +3,12 @@ import starImg from "../images/star.png";
 function Card(props) {
   return (
     <div className="card">
-      <img src={props.img} className="card--image" />
+      <img src={`../images/${props.img}`} className="card--image" />
       <div className="card--stats">
         <img src={starImg} className="card--star" />
         <span>{props.rating}</span>
         <span className="gray">({props.reviewCount}) • </span>
-        <span className="gray">{props.country}</span>
+        <span className="gray">{props.location}</span>
       </div>
       <p>{props.title}</p>
       <p>
