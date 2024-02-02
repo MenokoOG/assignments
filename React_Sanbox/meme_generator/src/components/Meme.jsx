37f92df9990e25f39971d91dd3386d13a5b90a90
Lawrence Jefferson II <jefftkddan@gmail.com>
2024-02-02 14:16:34 -0800

    V School Lesson update Menoko OG Date: 2-2-24

--- React_Sanbox/meme_generator/src/components/Meme.jsx
+++ React_Sanbox/meme_generator/src/components/Meme.jsx
@@ -1,24 +1,25 @@
 import React from "react";
-import memesData from "../memesData";
+import memesData from "../memesData.js";
 
 function Meme() {
-  let url
-    
-    const getMemeImage = () => {
-        const memesArray = memesData.data.memes
-        const randomNumber = Math.floor(Math.random() * memesArray.length)
-        url = memesArray[randomNumber].url
-        console.log(url)
-    }
-
+  const [memeImage, setMemeImage] = React.useState("");
+  
+  function getMemeImage() {
+    const memesArray = memesData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    setMemeImage(memesArray[randomNumber].url);
+  }
 
   return (
     <main>
       <div className="form">
         <input type="text" placeholder="Top text" className="form--input" />
-        <input type="text" placeholder="bottom text" className="form--input" />
-        <button className="form--button" onClick={getMemeImage}>Get a new meme image</button>
+        <input type="text" placeholder="Bottom text" className="form--input" />
+        <button className="form--button" onClick={getMemeImage}>
+          Get a new meme image 🖼
+        </button>
       </div>
+      <img src={memeImage} className="meme--image" />
     </main>
   );
 }
