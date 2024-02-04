013bf82d174f31cdf72b328ba3da7e8dba495ce9
Lawrence Jefferson II <jefftkddan@gmail.com>
2024-02-04 15:36:26 -0800

    code challenge udpate 2-4-24

--- code-challenges/daily-challenges/adjacent-difference/index.js
+++ code-challenges/daily-challenges/adjacent-difference/index.js
@@ -1,5 +1,19 @@
-function combined(){
-    // Your code here
+function combined(arr) {
+    let maxCombinedLength = 0;
+    let result = [];
+
+    for (let i = 0; i < arr.length - 2; i++) {
+        let combinedLength = arr[i].length + arr[i + 1].length + arr[i + 2].length;
+
+        if (combinedLength > maxCombinedLength) {
+            maxCombinedLength = combinedLength;
+            result = [arr[i], arr[i + 1], arr[i + 2]];
+        }
+    }
+
+    return result;
 }
 
+const resultArray = combined(["this", "is", "an", "array"]);
+console.log(resultArray);
 module.exports = { combined }
