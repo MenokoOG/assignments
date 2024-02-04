ce1cf949a845352fa63c60cdccd0971d6048025d
Lawrence Jefferson II <jefftkddan@gmail.com>
2024-02-04 15:19:55 -0800

    V School Lesson update Menoko OG Date: 2-4-24

--- level_3_react/ternary_practice/src/App.jsx
+++ level_3_react/ternary_practice/src/App.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { useState } from "react";
 
 function App() {
@@ -5,16 +6,23 @@ function App() {
    * Challenge: Replace the if/else below with a ternary
    * to determine the text that should display on the page
    */
-  const isGoingOut = true;
-
-  
- 
+  const [isGoingOut, setIsGoingOut] = useState(true);
+  /**
+   * Challenge:
+   * - Initialize state for `isGoingOut` as a boolean
+   * - Make it so clicking the div.state--value flips that
+   *   boolean value (true -> false, false -> true)
+   * - Display "Yes" if `isGoingOut` is `true`, "No" otherwise
+   */
+  function changeMind() {
+    setIsGoingOut((prevState) => !prevState);
+  }
 
   return (
     <div className="state">
       <h1 className="state--title">Do I feel like going out tonight?</h1>
-      <div className="state--value">
-        <h1>{isGoingOut === true ? "Yes" : "No"}</h1>
+      <div onClick={changeMind} className="state--value">
+        <h1>{isGoingOut ? "Yes" : "No"}</h1>
       </div>
     </div>
   );
