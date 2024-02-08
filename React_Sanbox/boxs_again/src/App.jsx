adda5ca06c0d9109524c18445052bf343642ec7d
Lawrence Jefferson II <jefftkddan@gmail.com>
2024-02-08 08:45:57 -0800

    V School Lesson update Menoko OG Date: 2-8-24

---
+++ React_Sanbox/boxs_again/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+// import Box from "./Box";
+import boxes from "./boxes";
+
+function App() {
+  
+
+  return (
+    <main>
+      <h1>Boxes go here</h1>
+    </main>
+  )
+}
+
+export default App
