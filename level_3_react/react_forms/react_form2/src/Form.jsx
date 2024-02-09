0baa58b1f2b9d3bc43cfa5cb277f2771fc74e072
Lawrence Jefferson II <jefftkddan@gmail.com>
2024-02-09 10:02:43 -0800

    V School Lesson update Menoko OG Date: 2-9-24

--- level_3_react/react_forms/react_form2/src/Form.jsx
+++ level_3_react/react_forms/react_form2/src/Form.jsx
@@ -1,14 +1,43 @@
-import React from "react";
+import { useState } from "react";
 
-function Form(){
-    return (
-        <form>
-            <input
-                type="text"
-                placeholder="First Name"
-            />
-        </form>
-    )
+function Form() {
+  const [formData, setFormData] = useState({firstName: "", lastName: "", email: ""});
+  
+console.log(formData)
+  
+
+  function handleChange(event) {
+    setFormData(prevFormData => {
+        return {
+            ...prevFormData,
+            [event.target.name]: event.target.value
+        }
+    })
+  }
+
+  
+
+  return (
+    <form>
+      <input
+        type="text"
+        placeholder="First Name"
+        onChange={handleChange}
+        name="firstName"
+      />
+      <input
+        type="text"
+        placeholder="Last Name"
+        onChange={handleChange}
+        name="lastName"
+      /><input
+      type="email"
+      placeholder="Email"
+      onChange={handleChange}
+      name="email"
+    />
+    </form>
+  );
 }
 
-export default Form
+export default Form;
