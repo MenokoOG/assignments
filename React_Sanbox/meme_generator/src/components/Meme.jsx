21da6c5a14bc76314d7ff1588eb2c3f6a0d416d2
Lawrence Jefferson II <jefftkddan@gmail.com>
2024-02-12 09:25:58 -0800

    V School Lesson update Menoko OG Date: 2-12-24

--- React_Sanbox/meme_generator/src/components/Meme.jsx
+++ React_Sanbox/meme_generator/src/components/Meme.jsx
@@ -7,21 +7,38 @@ function Meme() {
     randomImage: "http://i.imgflip.com/1bij.jpg"
   });
   const [allMemes, setAllMemes] = useState([]);
+/**
+    useEffect takes a function as its parameter. If that function
+    returns something, it needs to be a cleanup function. Otherwise,
+    it should return nothing. If we make it an async function, it
+    automatically retuns a promise instead of a function or nothing.
+    Therefore, if you want to use async operations inside of useEffect,
+    you need to define the function separately inside of the callback
+    function, as seen below:
+    */
 
-  useEffect(() => {
-    fetch("https://api.imgflip.com/get_memes")
-      .then(response => response.json())
-      .then(data => {
-        if (data.success) {
-          setAllMemes(data.data.memes);
-        } else {
-          console.error("Failed to fetch memes data");
-        }
-      })
-      .catch(error => {
-        console.error("Error fetching memes data:", error);
-      });
-  }, []); // Empty dependency array ensures this effect runs only once on component mount
+    useEffect(() => {
+      async function getMemes() {
+          const res = await fetch("https://api.imgflip.com/get_memes")
+          const data = await res.json()
+          setAllMemes(data.data.memes)
+      }
+      getMemes()
+  }, [])
+  // useEffect(() => {
+  //   fetch("https://api.imgflip.com/get_memes")
+  //     .then(response => response.json())
+  //     .then(data => {
+  //       if (data.success) {
+  //         setAllMemes(data.data.memes);
+  //       } else {
+  //         console.error("Failed to fetch memes data");
+  //       }
+  //     })
+  //     .catch(error => {
+  //       console.error("Error fetching memes data:", error);
+  //     });
+  // }, []); // Empty dependency array ensures this effect runs only once on component mount
 
   function getMemeImage() {
     const randomNumber = Math.floor(Math.random() * allMemes.length);
