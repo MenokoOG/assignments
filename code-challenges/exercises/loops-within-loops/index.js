bad5757278238b214b07a387ce4230425a9fcf57
Lawrence Jefferson II <jefftkddan@gmail.com>
2024-02-14 11:07:56 -0800

    V School Challenge update Menoko OG Date: 2-14-24

--- code-challenges/exercises/loops-within-loops/index.js
+++ code-challenges/exercises/loops-within-loops/index.js
@@ -1,8 +1,27 @@
-var people = ["Jon", "Jacob", "Jingle", "Heimer", "Schmidt"]
-var alphabet = "abcdefghijklmnopqrstuvwxyz"
+function forception(people, alphabet) {
+    let resultArray = [];
 
-function forception(people, alphabet){
-    // Your code here
+    for (let i = 0; i < people.length; i++) {
+        // Add the person's name followed by a colon
+        resultArray.push(people[i] + ":");
+
+        // Add all the uppercase letters of the alphabet
+        for (let j = 0; j < alphabet.length; j++) {
+            resultArray.push(alphabet[j].toUpperCase());
+        }
+    }
+
+    return resultArray;
 }
+
+// Test case
+var people = ["Jon", "Jacob", "Jingle", "Heimer", "Schmidt"];
+var alphabet = "abcdefghijklmnopqrstuvwxyz";
+console.log(forception(people, alphabet));
+
+
+
+
+
 
 module.exports = { forception }
