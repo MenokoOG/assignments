828d76c57faa3681893d719a0977e32695518ea1
Lawrence Jefferson II <jefftkddan@gmail.com>
2024-02-15 16:07:00 -0800

    update: 2-15-24

--- React_Sanbox/react_core_lessons/react-begin-lesson/src/App.jsx
+++ React_Sanbox/react_core_lessons/react-begin-lesson/src/App.jsx
@@ -3,6 +3,7 @@ import MyButton from './components/MyButton'
 import Profile from './components/Profile'
 import ShoppingList from './components/ShoppingList'
 import TwoButtons from './components/TwoButtons'
+import TwoButtonShare from './components/TwoButtonShare'
 
 
 function App() {
@@ -10,13 +11,16 @@ function App() {
 
   return (
     <>
-      <div>
-      <h1>Welcome to Menoko OG's React Practice</h1>
+      <div className='container'>
+      <h1 className='hello'>Welcome to Menoko OG's React Practice</h1>
       <p>React apps are made out of components. A component is a piece of the UI (user interface) that has its own logic and appearance. A component can be as small as a button, or as large as an entire page.</p>
+
+      <h2>Everything below this line is it's own component inported into the main app jsx file. How cool is that?</h2>
       <MyButton />
       <Profile />
       <ShoppingList />
       <TwoButtons />
+      <TwoButtonShare />
       </div>
       
     </>
