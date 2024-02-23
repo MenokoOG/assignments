51a134302463d1575aebb38ac3e5bbe7d02dbe76
Lawrence Jefferson II <jefftkddan@gmail.com>
2024-02-23 08:30:28 -0800

    update: 2-23-24

--- my_projects/crud_api/index.js
+++ my_projects/crud_api/index.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: false }));
 // CORS configuration
 // CORS configuration
 app.use(cors({
-  origin: 'http://127.0.0.1:5173'
+  origin: 'http://127.0.0.1:5174'
 }));
 
 
