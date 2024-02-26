30e5fed0600bdc6dd2a638d20e841340c680468f
Lawrence Jefferson II <jefftkddan@gmail.com>
2024-02-25 17:08:46 -0800

    V School workshop update Menoko OG Date: 2-25-24

--- workshops/server_setup_and_part2/client/src/App.jsx
+++ workshops/server_setup_and_part2/client/src/App.jsx
@@ -1,12 +1,30 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
+import axios from 'axios'
 
 function App() {
-  
+  const [allPosts, setAllPosts] = useState([])
 
+  function getPosts() {
+    axios.get("/api/posts")
+    .then(res => setAllPosts(res.data))
+    .catch(err => console.log(err))
+  }
+  useEffect(() => {
+    getPosts()
+  }, [])
+
+  const postList = allPosts.map(post => {
+    return (
+      <div>
+        <h1>{post.title}</h1>
+        <p>{post.description}</p>
+      </div>
+    )
+  })
   return (
     <>
-      
+      {postList}
     </>
   )
 }
