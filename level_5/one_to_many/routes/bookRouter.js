f79a8dda8ffd2464f4939b129f04d7496136f257
Lawrence Jefferson II <jefftkddan@gmail.com>
2024-03-05 09:33:52 -0800

    V School Lesson update Menoko OG Date: 3-5-24

--- level_5/one_to_many/routes/bookRouter.js
+++ level_5/one_to_many/routes/bookRouter.js
@@ -1,42 +1,68 @@
-const express = require("express")
-const bookRouter = express.Router()
-const Book = require("../models/books")
-
+const express = require("express");
+const bookRouter = express.Router();
+const Book = require("../models/books");
 
 // get all books
 bookRouter.get("/", (req, res, next) => {
-    
-    Book.find((err, books) => {
-        if(err){
-            res.status(500)
-            return next(err)
-        }
-        return res.status(200).send(books)
-    })
-})
+  Book.find((err, books) => {
+    if (err) {
+      res.status(500);
+      return next(err);
+    }
+    return res.status(200).send(books);
+  });
+});
 
 // get by author
 bookRouter.get("/:authorID", (req, res, next) => {
-    Book.find({author: req.params.authorID}, (err, books) => {
-        if (err) {
-            res.status(500)
-            return next(err)
-        }
-        return res.status(200).send(books)
-    })
-})
+  Book.find({ author: req.params.authorID }, (err, books) => {
+    if (err) {
+      res.status(500);
+      return next(err);
+    }
+    return res.status(200).send(books);
+  });
+});
 
 //add new book
 bookRouter.post("/:authorID", (req, res, next) => {
-    req.body.author = req.params.authorID
-    const newBook = new Book(req.body)
-    newBook.save((err, savedBook) => {
+  req.body.author = req.params.authorID;
+  const newBook = new Book(req.body);
+  newBook.save((err, savedBook) => {
+    if (err) {
+      res.status(500);
+      return next(err);
+    }
+    return res.status(201).send(savedBook);
+  });
+});
+
+// like a book
+bookRouter.put("/like/:bookID", (req, res, next) => {
+  Book.findOneAndUpdate(
+    { _id: req.params.bookID },
+    { $inc: { likes: 1 } },
+    { new: true },
+    (err, updatedBook) => {
+      if (err) {
+        res.status(500);
+        return next(err);
+      }
+      return res.status(201).send(updatedBook);
+    }
+  );
+});
+
+// find books by like range
+bookRouter.get("/search/bylikes", (req, res, next) => {
+    Book.where("likes").gte(5).exec((err, book) => {
         if (err) {
-            res.status(500)
-            return next(err)
-        }
-        return res.status(201).send(savedBook)
-    })
+            res.status(500);
+            return next(err);
+          }
+          return res.status(200).send(book);
+    }
+    )
 })
 
-module.exports = bookRouter
+module.exports = bookRouter;
