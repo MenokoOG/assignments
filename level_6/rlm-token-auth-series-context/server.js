be21121623442d78eae7077a09ed56b7831f8661
Lawrence Jefferson II <jefftkddan@gmail.com>
2024-03-13 10:40:37 -0700

    V School Lesson update Menoko OG Date: 3-13-24

--- level_6/rlm-token-auth-series-context/server.js
+++ level_6/rlm-token-auth-series-context/server.js
@@ -6,12 +6,12 @@ const mongoose = require('mongoose')
 const {expressjwt} = require('express-jwt')
 
 app.use(express.json())
+mongoose.set("strictQuery", true);
 app.use(morgan('dev'))
 
-mongoose.connect(
-  'mongodb://localhost:27017/user-authentication',
-  () => console.log('Connected to the DB')
-)
+mongoose.connect(process.env.DB_CONNECTION_STRING, (err) => {
+  console.log("connected to database", err);
+});
 
 app.use('/auth', require('./routes/authRouter.js'))
 app.use('/api', expressjwt({ secret: process.env.SECRET, algorithms: ['HS256'] })) // req.user
