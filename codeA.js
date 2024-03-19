7ba4339d3902504c4ec333af7627f0368a676d55
Lawrence Jefferson II <jefftkddan@gmail.com>
2024-03-19 16:10:04 -0700

    V School Lesson update Menoko OG Date: 3-19-24

--- codeA.js
+++ codeA.js
@@ -1,19 +1,4 @@
-function findSumOfTwo(numbers, target) {
-    for (let i = 0; i < numbers.length; i++) {
-      for (let j = i + 1; j < numbers.length; j++) {
-        if (numbers[i] + numbers[j] === target) {
-          return [numbers[i], numbers[j]];
-        }
-      }
-    }
-  
-    return [];
-}
 
-
-console.log(findSumOfTwo([2, 4, 7, 11, 15], 9)); // Output: [2, 7]
-
-console.log(findSumOfTwo([5, 12, 3, 9, 1], 8)); // Output: [3, 5]
 
 
 
