549d3d04d412cf44696819d01601e05636a3752f
Lawrence Jefferson II <jefftkddan@gmail.com>
2024-03-30 16:55:03 -0700

    update: 3-30-24

--- code-challenges/daily-challenges/array-methods-1/index.js
+++ code-challenges/daily-challenges/array-methods-1/index.js
@@ -1,8 +1,59 @@
+var peopleArray = [
+    {
+        firstName: "Sarah",
+        lastName: "Palin",
+        age: 47
+    },
+    {
+        firstName: "Frank",
+        lastName: "Zappa",
+        age: 12
+    },
+    {
+        firstName: "Rick",
+        lastName: "Sanchez",
+        age: 78
+    },
+    {
+        firstName: "Morty",
+        lastName: "Smith",
+        age: 29
+    },
+    {
+        firstName: "Kyle",
+        lastName: "Mooney",
+        age: 27
+    },
+    {
+        firstName: "Pasha",
+        lastName: "Datsyuk",
+        age: 13
+    },
+    {
+        firstName: "Lev",
+        lastName: "Tolstoy",
+        age: 82
+    }
+]
+
+
+
+
 function sortedOfAge(arr) {
-    // Return a list of everyone older than 18.
-    // Have the new list be sorted by last name.
-    // Return each name and age with <li>'s.
-}
+     // Filter people older than 18
+     const filteredPeople = arr.filter(person => person.age > 18);
+
+     // Sort by last name
+     filteredPeople.sort((a, b) => a.lastName.localeCompare(b.lastName));
+ 
+     // Map each person to a string containing HTML <li> element
+     const resultList = filteredPeople.map(person => `<li>${person.firstName} ${person.lastName} - Age: ${person.age}</li>`);
+ 
+     // Join the list items into a single string
+     const resultString = resultList.join('');
+ 
+     return resultString;
+ }
 
 module.exports = {
     sortedOfAge
