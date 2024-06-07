5224a59264140cce29483a38fb75aec0974b6609
Lawrence Jefferson II <jefftkddan@gmail.com>
2024-06-07 11:31:07 -0700

    6-7-24

--- code-challenges/exercises/map/index.js
+++ code-challenges/exercises/map/index.js
@@ -1,33 +1,41 @@
 ///Make an array of numbers that are doubles of the first array
 function doubleNumbers(arr){
+    return arr.map(num => num * 2);
 
 }
 
 // Take an array of numbers and make them strings
 function stringItUp(arr){
+    return arr.map(num => num.toString());
 
 }
 
 // Capitalize each of an array of names
-function capitalizeNames(arr){
-
+function capitalizeNames(arr) {
+    return arr.map(name => name.charAt(0).toUpperCase() + name.slice(1).toLowerCase());
 }
+
 
 // Make an array of strings of the names
 function namesOnly(arr){
-
+    return arr.map(person => person.name);
 }
 // Make an array of strings of the names saying whether or not they can go to The Matrix
 function makeStrings(arr){
-
+    return arr.map(person => {
+        if (person.age >= 18) {
+            return `${person.name} can go to The Matrix`;
+        } else {
+            return `${person.name} is under age!!`;
+        }
+    });
 }
 
 // Make an array of the names in h1s, and the ages in h2s
 function readyToPutInTheDOM(arr){
+    return arr.map(person => `<h1>${person.name}</h1><h2>${person.age}</h2>`);
 
 }
-
-
 module.exports = {
     doubleNumbers,
     stringItUp,
--- code-challenges/exercises/map/index.test.js
+++ code-challenges/exercises/map/index.test.js
@@ -11,29 +11,68 @@ describe("Array Mapping", () => {
     
     describe("Double Numbers function", () => {
         test("returns array with numbers doubled", () => {
-            expect(doubleNumbers([2, 5, 100])).toEqual([4, 10, 200])
+            expect(doubleNumbers([2, 5, 100])).toEqual([4, 10, 200]);
         })
-    })
+    });
 
-    // Remove 'x' in front of a 'describe' to have jest run the test on 'npm test'
-    xdescribe("String it Up function", () => {
- 
-    })
+    describe("String it Up function", () => {
+        test("returns array with numbers as strings", () => {
+            expect(stringItUp([2, 5, 100])).toEqual(["2", "5", "100"]);
+        })
+    });
 
-    xdescribe("Capitalize Names function", () => {
-    
-    })
+    describe("Capitalize Names function", () => {
+        test("returns array with capitalized names", () => {
+            expect(capitalizeNames(["john", "JANE", "sMiTh"])).toEqual(["John", "Jane", "Smith"]);
+        })
+    });
 
-    xdescribe("Names Only function", () => {
-     
-    })
+    describe("Names Only function", () => {
+        test("returns array of names from objects", () => {
+            const people = [
+                { name: "John", age: 25 },
+                { name: "Sarah", age: 32 },
+                { name: "Kevin", age: 17 },
+                { name: "Anna", age: 15 }
+            ];
+            expect(namesOnly(people)).toEqual(["John", "Sarah", "Kevin", "Anna"]);
+        })
+    });
 
-    xdescribe("Make Strings function", () => {
-        
-    })
+    describe("Make Strings function", () => {
+        test("returns array of strings indicating if they can go to The Matrix", () => {
+            const people = [
+                { name: "John", age: 25 },
+                { name: "Sarah", age: 32 },
+                { name: "Kevin", age: 17 },
+                { name: "Anna", age: 15 }
+            ];
+            const expected = [
+                "John can go to The Matrix",
+                "Sarah can go to The Matrix",
+                "Kevin is under age!!",
+                "Anna is under age!!"
+            ];
+            expect(makeStrings(people)).toEqual(expected);
+        })
+    });
 
-    xdescribe("Ready to put in the DOM function", () => {
-        
-    })
+    describe("Ready to put in the DOM function", () => {
+        test("returns array of strings with names in h1s and ages in h2s", () => {
+            const people = [
+                { name: "John", age: 25 },
+                { name: "Sarah", age: 32 },
+                { name: "Kevin", age: 17 },
+                { name: "Anna", age: 15 }
+            ];
+            const expected = [
+                "<h1>John</h1><h2>25</h2>",
+                "<h1>Sarah</h1><h2>32</h2>",
+                "<h1>Kevin</h1><h2>17</h2>",
+                "<h1>Anna</h1><h2>15</h2>"
+            ];
+            expect(readyToPutInTheDOM(people)).toEqual(expected);
+        })
+    });
 
-})
+});
