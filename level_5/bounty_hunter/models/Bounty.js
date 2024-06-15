5043dcd7ae44120ba16f43288efb0b8c5736d46e
Lawrence Jefferson II <jefftkddan@gmail.com>
2024-06-15 15:01:23 -0700

    change: 6-15-24

--- level_5/bounty_hunter/models/Bounty.js
+++ level_5/bounty_hunter/models/Bounty.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const bountySchema = new mongoose.Schema({
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
-    living: { type: Boolean, required: true },
+    living: { type: String, required: true },
     bountyAmount: { type: Number, required: true },
     type: { type: String, required: true }
 });
